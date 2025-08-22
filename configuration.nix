579ac0c0df03c8a4e7611168efca904ab152b073
Łukasz Tshipenchko <dev@zxc.sx>
2025-08-23 01:45:05 +0500

    Project Init

---
+++ configuration.nix
@@ -0,0 +1,42 @@
+{ modulesPath
+, lib
+, pkgs
+, ...
+} @ args:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    (modulesPath + "/profiles/qemu-guest.nix")
+    ./disk-config.nix
+  ];
+  boot.loader.grub = {
+    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+    # devices = [ ];
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+  services.openssh.enable = true;
+
+  networking.hostName = "gtw";
+  networking.useDHCP = false;
+  networking.interfaces.ens3.ipv4.addresses = [
+    {
+      address = "45.86.80.39";
+      prefixLength = 24;
+    }
+  ];
+  networking.defaultGateway = "45.86.80.1";
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
+
+  environment.systemPackages = map lib.lowPrio [
+    pkgs.curl
+    pkgs.gitMinimal
+  ];
+
+  users.users.root.openssh.authorizedKeys.keys =
+    [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMYcdiZTkmjVhqK+IEDv6Q9bSSyc7LkWK3vyfsPkVMen dev@zxc.sx"
+    ] ++ (args.extraPublicKeys or [ ]); # this is used for unit-testing this module and can be removed if not needed
+
+  system.stateVersion = "24.05";
+}
