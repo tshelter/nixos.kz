5da729cc5fc4fff51e32635a1885cc03bdbc430e
Łukasz Tshipenchko <dev@zxc.sx>
2025-08-23 12:26:50 +0500

    Remove facter

--- flake.nix
+++ flake.nix
@@ -2,12 +2,10 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.disko.url = "github:nix-community/disko";
   inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.nixos-facter-modules.url = "github:numtide/nixos-facter-modules";
 
   outputs =
     { nixpkgs
     , disko
-    , nixos-facter-modules
     , ...
     }:
     {
