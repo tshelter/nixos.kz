a537bdc04488e985bb103b831e12e48b09a6ff61
Yernar Tursynali <dev@zxc.sx>
2025-10-30 10:47:58 +0500

    Add cache.nixos.kz reverse proxy

--- nginx.nix
+++ nginx.nix
@@ -20,6 +20,21 @@
         return = "301 https://t.me/NixOSkz";
       };
     };
+
+    virtualHosts."cache.nixos.kz" = {
+      addSSL = true;
+      enableACME = true;
+      locations."/" = {
+        proxyPass = "https://cache.nixos.org";
+        extraConfig = ''
+          proxy_set_header Host cache.nixos.org;
+          proxy_ssl_server_name on;
+          proxy_redirect off;
+          proxy_buffering off;
+          proxy_request_buffering off;
+        '';
+      };
+    };
   };
 
   security.acme = {
