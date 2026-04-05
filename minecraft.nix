5a5890012dd6c509933fcfa75e44238528bca0d5
Yernar Tursynali <dev@zxc.sx>
2026-04-05 23:20:45 +0500

    Add hand7s to whitelist

--- minecraft.nix
+++ minecraft.nix
@@ -11,6 +11,7 @@
     declarative = true;
     whitelist = {
       dynzas = "69742bb1-d556-449e-bfcc-20fbf644e8db";
+      hand7s = "87adcacf-668b-4d17-a318-970c5b437d9a";
     };
     package = pkgs.minecraftServers.vanilla-1-20;
     serverProperties = {
