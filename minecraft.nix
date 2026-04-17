444091c933fedacaf0eb00a14cc59b07ee42d9bd
Yernar Tursynali <dev@zxc.sx>
2026-04-17 21:16:49 +0500

    Add new user

--- minecraft.nix
+++ minecraft.nix
@@ -10,8 +10,10 @@
     openFirewall = true;
     declarative = true;
     whitelist = {
+      # curl -s https://api.mojang.com/users/profiles/minecraft/dynzas | jq -r '.id | "\(.[0:8])-\(.[8:12])-\(.[12:16])-\(.[16:20])-\(.[20:])"'
       dynzas = "69742bb1-d556-449e-bfcc-20fbf644e8db";
       hand7s = "87adcacf-668b-4d17-a318-970c5b437d9a";
+      venix756 = "498d50dc-f90a-4889-ac4c-878b20e98a17";
     };
     package = pkgs.minecraftServers.vanilla-1-20;
     serverProperties = {
